adc16a65396f325fc63e2312648d86b199ff7185
aryankarma <aryankarma29@gmail.com>
2023-10-17 06:35:22 +0000

    anime.js

--- weatherapp/app/weatherCurrent/page.jsx
+++ weatherapp/app/weatherCurrent/page.jsx
@@ -27,16 +27,16 @@ export default function Home() {
 
   window.onload = function(){
     console.log("console")
-    var motion_path = anime.path('#svgID'); 
-    anime({
-        targets: '#box',
-        translateX: motion_path('x'), 
-        translateY: motion_path('y'),
-        // rotate: motion_path('angle'),
-        easing: 'linear',
-        duration: 10000,
-        loop: true,
-    });
+    // var motion_path = anime.path('#svgID'); 
+    // anime({
+    //     targets: '#box',
+    //     translateX: motion_path('x'), 
+    //     translateY: motion_path('y'),
+    //     rotate: motion_path('angle'),
+    //     easing: 'linear',
+    //     duration: 10000,
+    //     loop: true,
+    // });
   }
 
   // fetching api
@@ -85,7 +85,7 @@ export default function Home() {
         <svg viewBox="0 0 385 87" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path id='svgID' d="M1.5 86C17.6667 59.3334 76.4 4.39998 184 1.99998C226.5 1.25021e-05 322.5 8.5 383.5 85" stroke="red" strokeWidth="1" />
         </svg>
-        <img className={styles.img} src="/images/Ellipse11.png" alt="xy" />
+        <img className={styles.img} src="/images/Ellipse14.png" alt="xy" />
       </div>
       {/* <div id='boxContaier' className={styles.boxContaier}> */}
         <div className={styles.box} id="box"></div>
