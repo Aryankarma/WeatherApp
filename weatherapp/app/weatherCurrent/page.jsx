c6a7f8c4564652ae9ed331ef53ad71d304b2eb13
aryankarma <aryankarma29@gmail.com>
2023-10-23 11:58:46 +0000

    sunAnime

--- weatherapp/app/weatherCurrent/page.jsx
+++ weatherapp/app/weatherCurrent/page.jsx
@@ -1,54 +1,34 @@
 "use client";
-import React from 'react'
-import { useState, useEffect } from 'react'
-import styles from './weatherCurrent.module.css'
-import anime from 'animejs/lib/anime.es.js';
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from "./weatherCurrent.module.css";
+import anime from "animejs/lib/anime.es.js";
 
 export default function Home() {
-
   const api = "24951e153ffc4135aeb175518231307";
 
   const formSubmit = (e) => {
     e.preventDefault();
-    const rawData = new FormData(e.currentTarget)
-    const dataInput = rawData.get("city")
-    console.log(dataInput)
-    fetchWeather(api, dataInput)
-  }
-  
-  // working with animations
-  // var elements = document.getElementById("aqiRange");
-  // anime({
-  //     targets: elements,
-  //     translateX: 270
-  // });
-
-  window.onload = function(){
-    console.log("console")
-    // var motion_path = anime.path('#svgID'); 
-    // anime({
-    //     targets: '#box',
-    //     translateX: motion_path('x'), 
-    //     translateY: motion_path('y'),
-    //     rotate: motion_path('angle'),
-    //     easing: 'linear',
-    //     duration: 10000,
-    //     loop: true,
-    // });
-  }
+    const rawData = new FormData(e.currentTarget);
+    const dataInput = rawData.get("city");
+    console.log(dataInput);
+    fetchWeather(api, dataInput);
+  };
 
   // fetching api
-  // useEffect(()=>{
   const fetchWeather = async (api, city) => {
-    const url = "https://api.weatherapi.com/v1/current.json?key=" + api + "&q=" + city + "&aqi=yes";
+    const url =
+      "https://api.weatherapi.com/v1/current.json?key=" +
+      api +
+      "&q=" +
+      city +
+      "&aqi=yes";
     try {
-      const rawApi = await fetch(url)
+      const rawApi = await fetch(url);
       const data = await rawApi.json();
-      console.log(data)
+      console.log(data);
       let x = data.current.air_quality.pm2_5;
-      updateValue(x)
+      updateValue(x);
       /*
       AQI = (PM2.5 - BPLo) / (BPhi - BPLo) * (IHi - ILo) + ILo
       PM2.5 = x   BPLo = 12 μg/m³   ILo = 50  IHi = 100    BPhi = 35 μg/m³
@@ -62,36 +42,119 @@ export default function Home() {
         301-500: Hazardous
      */
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-  // fetchWeather();
-  // }, [])
-
-  const [value1, updateValue] = useState(55)
-  return <div className='homeContainer p-10 text-center justify-center items-center flex flex-col ...' >
-    
-    <h1 className='p-5 text-3xl font-extrabold'>Weather Forecast</h1>
-    
-    <form action="" onSubmit={(e) => formSubmit(e)}>
-      <input autoFocus className='outline text-black rounded outline-2 p-5 w-1/2' type="text" placeholder='enter your city' name='city' />
-      <button className='btn p-5' type="submit">Search</button>
-    </form>
-    
-    <input max={500} value={value1} onChange={(e) => updateValue(e.target.value)} className={styles.aqiRange} type="range" name="aqi" id="aqiRange" />
-    
-    <div className={styles.mainSvgContainer}>
-      <div className={styles.svgContainer}>
-        <svg viewBox="0 0 385 87" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path id='svgID' d="M1.5 86C17.6667 59.3334 76.4 4.39998 184 1.99998C226.5 1.25021e-05 322.5 8.5 383.5 85" stroke="red" strokeWidth="1" />
-        </svg>
-        <img className={styles.img} src="/images/Ellipse14.png" alt="xy" />
-      </div>
-      {/* <div id='boxContaier' className={styles.boxContaier}> */}
-        <div className={styles.box} id="box"></div>
-      {/* </div> */}
-    </div>
+  };
   
+  const [value1, updateValue] = useState(55);
 
-  </div>
+  // document.getElementById('circlePath').style.transform = `rotate(${20}deg)`;
+
+  useEffect(()=>{
+    // Get the CSS element that you want to change the animation for.
+    
+    // const element = document.getElementById('weatherCurrent_circlePath__WfQJv');
+    
+    // element.getAnimations()[0].effect.target.style.rotate = "50deg";
+    // console.log(element.getAnimations()[0].effect.target.style.rotate)
+    // console.log(element.getAnimations())
+    // console.log(element.style.margin)
+    // Get the current animation transform `rotate()` degree value.
+    // const rotateValue = element.style.transform.match(/rotate\(([^)]+)\)/)[0];
+
+    // Set the new animation transform `rotate()` degree value.
+    // element.style.transform = `rotate(${30}deg)`;
+
+    // // const oldSelect = document.getElementsByClassName("chngDeg");  
+    // name.style.transform = `rotate(${20}deg)`;
+    // // const newSelect = document.getElementById('weatherCurrent_circlePath__WfQJv');
+    // // newSelect.style.transform = 'rotate(${60}deg)'
+
+    const element = document.querySelector('#weatherCurrent_circlePath__WfQJv');
+    console.log(element.style)
+    // const animationName = getComputedStyle(element).getPropertyValue('animation-name');
+    // const animationName2 = getComputedStyle(element).getPropertyValue("transform")
+    // matrix(0.983272, 0.182145, -0.182145, 0.983272, 0, 0)
+    // const final = animationName2.slice(7,14)
+    // const animationName3 = final*(180 / Math.PI);
+    // console.log("animationName2 " + animationName2)
+    // console.log("animationName3 " +animationName3)
+    // console.log("final " + final)
+    
+
+    // anime js animation functions 
+
+    // let degValue = "12deg" 
+    // const alter = "alternate"
+
+    // animation for main sun container rotation
+    
+    anime({
+      targets: ".chngDeg",
+      rotate: "60deg",
+      // rotate: degValue,
+      duration: 3000,
+      easing:"easeInOutQuad",
+      loop: true,
+      elasticity:200,
+      // direction: "alternate",
+      // direction: alter,
+    })
+
+  })
+  
+  // window.onload = function(){
+  //   console.log("after loading")
+  //   const element = document.querySelector('#weatherCurrent_circlePath__WfQJv');
+  //   // thisis printing the rotate value at 0% but we need to modify the rotation value at 100%
+  //   element.style.rotate = "0deg" ;
+  //   console.log(element.style.rotate)
+  // }
+
+  return (
+    <div className="homeContainer p-10 text-center justify-center items-center flex flex-col ...">
+      <h1 className="p-5 text-3xl font-extrabold">Weather Forecast</h1>
+
+      <form action="" onSubmit={(e) => formSubmit(e)}>
+        <input
+          autoFocus
+          className="outline text-black rounded outline-2 p-5 w-1/2"
+          type="text"
+          placeholder="enter your city"
+          name="city"
+        />
+        <button className="btn p-5" type="submit">
+          Search
+        </button>
+      </form>
+
+      <input
+        max={500}
+        value={value1}
+        onChange={(e) => updateValue(e.target.value)}
+        className={styles.aqiRange}
+        type="range"
+        name="aqi"
+        id="aqiRange"
+      />
+
+      <div className={styles.sunContainer}>
+
+        <div className="chngDeg" id={styles.circlePath}>
+         <div className={styles.item}></div>
+        </div>
+
+        <img className={styles.img} src="images/Ellipsefinal.png" alt="sunLine" />
+        <div className={styles.blackShade}>
+
+          <div className="timings sunrise"></div>
+          <div className="timings sunset"></div>
+
+        </div>
+
+      </div>
+
+
+    </div>
+  );
 }
