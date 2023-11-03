9d98559bfe444c8431c63a7c310bdf739e4420d0
Aryan Karma <110737632+Aryankarma@users.noreply.github.com>
2023-11-03 17:39:37 +0000

    connecting api v1.0.0

--- app/weatherCurrent/page.jsx
+++ app/weatherCurrent/page.jsx
@@ -33,18 +33,45 @@ export default function Home() {
 
   // fetching api
   const fetchWeather = async (api, city) => {
-    const url =
+    const urlCurrent =
       "https://api.weatherapi.com/v1/current.json?key=" +
       api +
       "&q=" +
       city +
       "&aqi=yes";
+    const urlAstro = 
+    "https://api.weatherapi.com/v1/astronomy.json?key=" +
+    api +
+    "&q=" +
+    city +
+    "&aqi=yes";
     try {
-      const rawApi = await fetch(url);
-      const data = await rawApi.json();
-      console.log(data);
-      let x = data.current.air_quality.pm2_5;
+
+      const currentData = await fetch(urlCurrent);
+      const currentDataFinal = await currentData.json();
+
+      const astroData = await fetch(urlAstro);
+      const astroDataFinal = await astroData.json();
+
+      console.log(currentDataFinal);
+      console.log(astroDataFinal);
+      
+      const sunrise = astroDataFinal.astronomy.astro.sunrise;
+      const sunset = astroDataFinal.astronomy.astro.sunset;
+      setSunsetTime(sunset.toLowerCase())
+      setSunriseTime(sunrise.toLowerCase())
+
+      let x = currentDataFinal.current.air_quality.pm2_5;
+      let y = currentDataFinal.location.localtime;
       updateValue(x);
+
+      const slicefrom = y.length - (y.length - (length-5));
+      const currentTime = y.slice(slicefrom, y.length);
+
+      console.log(currentTime)
+
+      updateDegValue("60deg")
+      fucnt();
       /*
       AQI = (PM2.5 - BPLo) / (BPhi - BPLo) * (IHi - ILo) + ILo
       PM2.5 = x   BPLo = 12 μg/m³   ILo = 50  IHi = 100    BPhi = 35 μg/m³
@@ -57,67 +84,37 @@ export default function Home() {
         201-300: Very Unhealthy
         301-500: Hazardous
      */
-    } catch (error) {
+    
+      } catch (error) {
       console.log(error);
     }
   };
   
   const [value1, updateValue] = useState(55);
+  const [sunriseTime, setSunriseTime] = useState("")
+  const [sunsetTime, setSunsetTime] = useState("")
 
-  // document.getElementById('circlePath').style.transform = `rotate(${20}deg)`;
 
-  useEffect(()=>{
-    // Get the CSS element that you want to change the animation for.
+  // useEffect(()=>{
+    const [degValue, updateDegValue] = useState(0)
+
+    const fucnt=()=>{
+
+      const element = document.querySelector('#weatherCurrent_circlePath__WfQJv');
+      console.log(element.style.transform = "rotate(0deg)");
+
+      anime({
+        targets: ".chngDeg",
+        rotate: degValue,
+        duration: 3000,
+        easing:"easeInOutQuad",
+        // loop: true,
+        elasticity:200
+      })
+    } 
+    // })
+
     
-    // const element = document.getElementById('weatherCurrent_circlePath__WfQJv');
-    
-    // element.getAnimations()[0].effect.target.style.rotate = "50deg";
-    // console.log(element.getAnimations()[0].effect.target.style.rotate)
-    // console.log(element.getAnimations())
-    // console.log(element.style.margin)
-    // Get the current animation transform `rotate()` degree value.
-    // const rotateValue = element.style.transform.match(/rotate\(([^)]+)\)/)[0];
-
-    // Set the new animation transform `rotate()` degree value.
-    // element.style.transform = `rotate(${30}deg)`;
-
-    // // const oldSelect = document.getElementsByClassName("chngDeg");  
-    // name.style.transform = `rotate(${20}deg)`;
-    // // const newSelect = document.getElementById('weatherCurrent_circlePath__WfQJv');
-    // // newSelect.style.transform = 'rotate(${60}deg)'
-
-    const element = document.querySelector('#weatherCurrent_circlePath__WfQJv');
-    // console.log(element.style)
-    // const animationName = getComputedStyle(element).getPropertyValue('animation-name');
-    // const animationName2 = getComputedStyle(element).getPropertyValue("transform")
-    // matrix(0.983272, 0.182145, -0.182145, 0.983272, 0, 0)
-    // const final = animationName2.slice(7,14)
-    // const animationName3 = final*(180 / Math.PI);
-    // console.log("animationName2 " + animationName2)
-    // console.log("animationName3 " +animationName3)
-    // console.log("final " + final)
-    
-
-    // anime js animation functions 
-
-    // let degValue = "12deg" 
-    // const alter = "alternate"
-
-    // animation for main sun container rotation
-    
-    anime({
-      targets: ".chngDeg",
-      rotate: "60deg",
-      // rotate: degValue,
-      duration: 3000,
-      easing:"easeInOutQuad",
-      loop: true,
-      elasticity:200,
-      // direction: "alternate",
-      // direction: alter,
-    })
-
-  })
 
   return (
     <div className={styles.homeContainer}>
@@ -164,7 +161,7 @@ export default function Home() {
                 <p>Sunrise</p>
               </div>
                 <hr style={{border:0}} />
-                <p id={styles.riseTime}>5:00am</p>
+                <p id={styles.riseTime}>{sunriseTime}</p>
             </div>
             <div className={styles.sunset}>
             <div className={styles.upper}>
@@ -172,7 +169,7 @@ export default function Home() {
                 <p>Sunset</p>
             </div>
                 <hr style={{border:0}}/>
-                <p id={styles.setTime}>9:12pm</p>
+                <p id={styles.setTime}>{sunsetTime}</p>
             </div>
           </div>
         </div>
