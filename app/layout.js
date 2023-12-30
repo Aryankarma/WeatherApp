1c982aedbaf5b817c4f333c02f41648c0849be05
Aryan Karma <110737632+Aryankarma@users.noreply.github.com>
2023-12-30 08:13:34 +0000

    web analyrics

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
       {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js" integrity="sha512-z4OUqw38qNLpn1libAN9BsoDx6nbNFio5lA6CuTp9NlK83b89hgyCVq+N5FdBJptINztxn1Z3SaKSKUS5UP60Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> */}
       <body className={inter.className}>
           <SpeedInsights/>
+          
           {children}
         </body>
     </html>
